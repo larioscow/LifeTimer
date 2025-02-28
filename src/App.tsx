2a6ae45003190052a7ad03fa15c87b7567d81719
larioscow <christiandlf15@gmail.com>
2025-02-28 11:08:15 -0600

    v0

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Timer, Task } from './components';
+import { Timer } from './components';
 import { Card } from './components/UI/card';
 
 const App = () => {
   return (
-    <main className="h-screen flex justify-center items-center bg-amber-100">
-      <div className="flex flex-col items space-y-2">
+    <main className="h-screen flex items-start justify-center py-10 bg-amber-100 text-amber-950">
+      <div className="flex flex-col w-2/3 space-y-2 ">
         <Card>
-          <Timer className="text-5xl font-bold antialiased font-dm" />
+          <Timer className="flex-center text-5xl font-bold antialiased font-dm" />
         </Card>
-        <Task name="Do the dishes" startHour="9:00" endHour="11:00"></Task>
+        <Card>
+          <h2>Tasks</h2>
+        </Card>
+        <Card>
+          <h2>Pomodoro</h2>
+        </Card>
       </div>
     </main>
   );
