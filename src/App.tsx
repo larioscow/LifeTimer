b85b6e19da19563656c5e9d5b5d09705232e6c3e
larioscow <christiandlf15@gmail.com>
2025-03-19 12:44:22 -0600

    bug correction

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import useMenuStore from './stores/useMenuStore';
 const App = () => {
   const { isMenuOpen } = useMenuStore();
   return (
-    <main className="h-[100dvh] flex items-start justify-center py-10 no-scrollbar dark:bg-black">
+    <main className="h-[100dvh] flex justify-center py-10 no-scrollbar dark:bg-black">
       {isMenuOpen && <Menu />}
       {!isMenuOpen && <Tasks />}
       <div className="flex flex-col items-center space-y-1.5 absolute bottom-5 right-5">
