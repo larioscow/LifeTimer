9ae4d14d2320be924ecfdd5a7ce809e9bb1ac89e
larioscow <christiandlf15@gmail.com>
2025-04-11 16:38:00 -0600

    api correction

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -12,14 +12,17 @@ export const Register = () => {
 
   const handleRegister = async () => {
     try {
-      const res = await axios.post('https://lifetimer.larioscow.dev/register', {
-        username: username,
-        password: password,
-      });
+      const res = await axios.post(
+        'https://life-timer-api.larioscow.dev/register',
+        {
+          username: username,
+          password: password,
+        }
+      );
       if (res.status === 200) {
         //login
         const loginRes = await axios.post(
-          'https://lifetimer.larioscow.dev/login',
+          'https://life-timer-api.larioscow.dev/login',
           {
             username: username,
             password: password,
